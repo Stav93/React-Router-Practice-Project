d6b7ecaa81f506c3e60301c98567b028f96e7d99
Stav <stavush93@gmail.com>
2022-11-12 22:10:08 +0200

    adding nested routes

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments"
 
 function QuoteDetail() {
   const params = useParams();
@@ -8,6 +10,10 @@ function QuoteDetail() {
     <>
       <h1>QuoteDetail</h1>
       <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments/>
+      </Route>
+      {/* <Route path="/quotes/:quoteId/comments"></Route> */}
     </>
   );
 }
