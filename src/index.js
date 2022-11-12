c0b968d78070c9ebcb29945b457b24d3c3eb38c5
Stav <stavush93@gmail.com>
2022-11-12 21:40:20 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-=======
-import ReactDOM from 'react-dom/client';
->>>>>>> ad31ed309b43a08c6226805fd5dbe07bd6923dcd
 
 import "./index.css";
 import App from "./App";
