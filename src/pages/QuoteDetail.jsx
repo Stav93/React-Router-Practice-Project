e9e0519c1194957051e3c41ba192fc685e4a31fc
Stav <stavush93@gmail.com>
2022-11-13 19:07:37 +0200

    navigate to a specific quote

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { useParams, Route } from "react-router-dom";
 
-import Comments from "../components/comments/Comments"
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import Comments from "../components/comments/Comments";
 
 function QuoteDetail() {
   const params = useParams();
 
+  const Quotes_Example = [
+    { id: "q1", author: "Max", text: "Learning React Router" },
+    { id: "q2", author: "Stav", text: "Learning React Router2" },
+  ];
+
+  const quote = Quotes_Example.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found</p>
+  }
+
   return (
     <>
-      <h1>QuoteDetail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
-        <Comments/>
+        <Comments />
       </Route>
       {/* <Route path="/quotes/:quoteId/comments"></Route> */}
     </>
