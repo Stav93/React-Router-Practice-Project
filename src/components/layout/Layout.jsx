3362dd977cdd3946617a1e6f8b818509cd9f7e16
Stav <stavush93@gmail.com>
2022-11-13 18:46:52 +0200

    added navigation using NavLink comp.

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainNavigation from "./MainNavigation"
+import classes from "./Layout.module.css"
+
+function Layout( {children} ) {
+  return (
+    <>
+      <MainNavigation />
+      <main className={classes.main}>
+        {children}
+      </main>
+    </>
+  )
+}
+
+export default Layout
