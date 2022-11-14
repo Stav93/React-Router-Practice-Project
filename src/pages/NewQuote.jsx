c84d884920efe9e7c80351509ec89b442895a9a4
Stav <stavush93@gmail.com>
2022-11-14 21:21:07 +0200

    use useHistory() hook

--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useHistory } from "react-router-dom"
 import QuoteForm from "../components/quotes/QuoteForm"
 
 function NewQuote() {
 
+  const historyObj = useHistory();
+
   const addQuoteHandler = quoteData => {
     console.log(quoteData)
+    historyObj.push("/quotes")
   }
   return (
     <QuoteForm onAddQuote={addQuoteHandler}/>
