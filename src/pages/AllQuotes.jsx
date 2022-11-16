44de93beb9f2743758a5ac008dc029de84a2f68a
Stav <stavush93@gmail.com>
2022-11-16 22:43:36 +0200

    loading all quotes and detail quotes using http

--- src/pages/AllQuotes.jsx
+++ src/pages/AllQuotes.jsx
@@ -1,5 +1,9 @@
-import React from "react";
-import QuoteList from "../components/quotes/QuoteList"
+import React, { useEffect } from "react";
+import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound"
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
 
 const Quotes_Example = [
   { id: "q1", author: "Max", text: "Learning React Router" },
@@ -7,7 +11,35 @@ const Quotes_Example = [
 ];
 
 function AllQuotes() {
-  return <QuoteList quotes={Quotes_Example}/>;
+  const {
+    sendRequest,
+    status,
+    data: LoadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centeres">
+        {" "}
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === "completed" && (!LoadedQuotes || LoadedQuotes.length === 0)) {
+    return <NoQuotesFound/>
+  }
+
+  return <QuoteList quotes={LoadedQuotes} />;
 }
 
 export default AllQuotes;
--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
-
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+import LoadingSpinner from "../components/UI/LoadingSpinner"
+import useHttp from "../hooks/use-http"
+import { getSingleQuote } from "../lib/api"
+
 
 function QuoteDetail() {
   const match = useRouteMatch();
   const params = useParams();
 
-  const Quotes_Example = [
-    { id: "q1", author: "Max", text: "Learning React Router" },
-    { id: "q2", author: "Stav", text: "Learning React Router2" },
-  ];
+  // const Quotes_Example = [
+  //   { id: "q1", author: "Max", text: "Learning React Router" },
+  //   { id: "q2", author: "Stav", text: "Learning React Router2" },
+  // ];
 
-  const quote = Quotes_Example.find((quote) => quote.id === params.quoteId);
+  const { sendRequest, status, data: LoadedQuote, error } = useHttp(getSingleQuote, true)
 
-  if (!quote) {
+  // const quote = Quotes_Example.find((quote) => quote.id === params.quoteId);
+
+  // if (!quote) {
+  //   return <p>No Quote Found</p>;
+  // }
+
+  const { quoteId } = params;
+
+  useEffect(() => {
+    sendRequest(quoteId)
+  }, [sendRequest, quoteId])
+
+  if (status === "pending") {
+    return <div className="centered">
+      <LoadingSpinner/>
+    </div>
+  }
+
+  if (error) {
+    return <p className="centered">
+     {error}
+    </p>
+  }
+
+  if (!LoadedQuote.text) {
     return <p>No Quote Found</p>;
   }
 
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={LoadedQuote.text} author={LoadedQuote.author} />
       <Route path={`${match.path}`} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
