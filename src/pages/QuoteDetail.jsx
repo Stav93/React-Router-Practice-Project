d8598c56324064e32734636c67437818654f539b
Stav <stavush93@gmail.com>
2022-11-16 21:31:05 +0200

    adding a Link to comment page with a btn

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
@@ -15,12 +15,19 @@ function QuoteDetail() {
   const quote = Quotes_Example.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
-    return <p>No Quote Found</p>
+    return <p>No Quote Found</p>;
   }
 
   return (
     <>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
