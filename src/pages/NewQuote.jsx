4bb341e1499d8186b063227b78445e7fe163ec71
Stav <stavush93@gmail.com>
2022-11-16 22:21:56 +0200

    adding an new quote and send an http request

--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useHistory } from "react-router-dom"
 import QuoteForm from "../components/quotes/QuoteForm"
+import useHttp from "../hooks/use-http"
+import { addQuote } from "../lib/api"
 
 function NewQuote() {
-
+  const {sendRequest, status} = useHttp(addQuote);
   const historyObj = useHistory();
 
+  useEffect(() => {
+    if (status === "completed") {
+      historyObj.push("/quotes")
+    }
+  }, [status, historyObj])
+
   const addQuoteHandler = quoteData => {
-    console.log(quoteData)
-    historyObj.push("/quotes")
+    sendRequest(quoteData);
+    // historyObj.push("/quotes")
   }
   return (
-    <QuoteForm onAddQuote={addQuoteHandler}/>
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler}/>
   )
 }
 
