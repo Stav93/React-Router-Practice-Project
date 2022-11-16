0b05da65b508e51bb42c747c15e29dad9d8217d9
Stav <stavush93@gmail.com>
2022-11-16 22:13:02 +0200

    adding firebase database

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const FIREBASE_DOMAIN = 'https://react-prep-default-rtdb.firebaseio.com';
+const FIREBASE_DOMAIN = 'https://react-router-26fd2-default-rtdb.firebaseio.com/';
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
